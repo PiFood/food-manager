f14896f1c3201a872580c4cc799a07c142e013a8
Ethan Zhao <ethanxyzhao@gmail.com>
2016-01-28 08:53:50 -0600

    Update food-manager.py

--- a-4th-grader-is-helping.-Give-me-credit-D/food-manager.py
+++ a-4th-grader-is-helping.-Give-me-credit-D/food-manager.py
@@ -3,14 +3,14 @@ import time
 while 21:
     foodinput = raw_input("What is the food: ")
     foodinput = foodinput.lower()
-    def Calculator(name, sving_size, fat, calories):
-        print "It is a(n) " + name + " with " + str(sving_size) + "g" + " as the serving size and " + str(fat) + "g" + " and with + "str(calories) + " calories."
+    def Calculator(name, sving_size, sving_size_unit, fat, calories):
+        print "It is a(n) " + name + " with " + str(sving_size) + str(sving_size_unit) + " as the serving size and " + str(fat) + "g" + " of fat and with " + str(calories) + " calories."
     
     if len(foodinput) > 0 and foodinput.isalpha():
         if foodinput == "apple":
-            Calculator("Apple", 1, 30, 300)
+            Calculator("apple", 1, fruit, 30, 300)
         elif foodinput == "pear":
-            Calculator("Pear", 2, 35, 450)
+            Calculator("pear", 2, fruit, 35, 450)
         else:
             print "Can't recognize."
     
