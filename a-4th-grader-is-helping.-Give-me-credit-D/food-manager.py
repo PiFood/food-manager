d247058078b55fce8fb4496dc32328043b2ab2d9
Ethan Zhao <ethanxyzhao@gmail.com>
2016-01-28 08:39:48 -0600

    Update food-manager.py

--- a-4th-grader-is-helping.-Give-me-credit-D/food-manager.py
+++ a-4th-grader-is-helping.-Give-me-credit-D/food-manager.py
@@ -4,12 +4,15 @@ while 21:
     foodinput = raw_input("What is the food: ")
     foodinput = foodinput.lower()
     def Calculator(name, sving_size, fat, calories):
-        print "It is " + name + " Serving = " + str(sving_size) + "g" + " Fat = " + str(fat) + " Calories = " + str(calories)
-    if foodinput == "apple":
-        Calculator("apple", 1, 30, 300)
-        
-
-    if len(foodinput) == 0:
-        print "Empty. Please enter a food."
+        print "It is a(n)" + name + " with a serving size of = " + str(sving_size) + "g" + " Fat = " + str(fat) + " Calories = " + str(calories)
+    
+    if len(foodinput) > 0 and foodinput.isalpha():
+        if foodinput == "apple":
+            Calculator("Apple", 1, 30, 300)
+        elif foodinput == "pear":
+            Calculator("pear", 2, 35, 450)
+        else:
+            print "Can't recognize."
+    
     else:
-         print "Successful!"  
+        print "Empty. Please enter a food."
