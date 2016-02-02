3a3df105e5dc1216247d7f43304b393267d14fa9
ericwxyang <sirericy@gmail.com>
2016-02-01 18:05:51 -0600

    Update food-manager.py

--- a-4th-grader-is-helping.-Give-me-credit-D/food-manager.py
+++ a-4th-grader-is-helping.-Give-me-credit-D/food-manager.py
@@ -4,43 +4,10 @@ import time
 #!/usr/bin/python
 from sys import argv, exit
 import zbar
-def scanner():
-    # create a Processor
-    proc = zbar.Processor()
-    
-    # configure the Processor
-    proc.parse_config('enable')
-    
-    # initialize the Processor
-    device = '/dev/video0'
-    if len(argv) > 1:
-        device = argv[1]
-    proc.init(device)
-    
-    # setup a callback
-    def my_handler(proc, image, closure):
-        # extract results
-        for symbol in image.symbols:
-            # do something useful with results
-            print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
-            if symbol.data == "381371151035":
-                print "Aveeno"
-    proc.set_data_handler(my_handler)
-    
-    # enable the preview window
-    proc.visible = True
-    
-    # initiate scanning
-    proc.active = True
-    try:
-        # keep scanning until user provides key/mouse input
-        proc.user_wait()
-    except zbar.WindowClosed, e:
-        pass
-def enter():
 
-    foodinput = raw_input("What is the food? ")
-    foodinput = foodinput.lower()
+global foodinput
+
+def search():
     def Calculator(name, sving_size, sving_size_unit, calories, total_fat, sat_fat, trans_fat):
         print "It is a(n) " + name + " with " + str(sving_size) + " " + str(sving_size_unit) + " as the serving size, " + str(calories) + " calories, and " + str(total_fat) + "g of total fat."
         print "This includes " + str(sat_fat) + "g of saturated fat and " + str(trans_fat) + "g of trans fat."
@@ -49,7 +16,7 @@ def enter():
             
 #FritoLay        
     #Cheetos
-        if foodinput == "cheetos crunchy" or "cheetos":
+        if foodinput == "cheetos crunchy":
             Calculator("Cheetos Crunchy", 1, "oz (28g/about 21 pieces)", 150, 10, 1.5, 0)
         elif foodinput == "cheetos puffs":
             Calculator("Cheetos Puffs", 1, "oz (28g/about 21 pieces)", 150, 10, 1.5, 0)
@@ -59,9 +26,9 @@ def enter():
         elif foodinput == "doritos nacho cheese":
             Calculator("Doritos Nacho Cheese", 1, "oz (28g/about 11 pieces)", 140, 8, 1, 0)
     #Lay's
-        elif foodinput == "lays barbecue" or "lay's barbecue":
+        elif foodinput == "lays barbecue":
                 Calculator("Lay's Barbecue", 1, "oz (28g/about 15 pieces)", 160, 10, 1.5, 0)
-        elif foodinput == "lays classic" or "lay's classic":
+        elif foodinput == "lays classic":
             Calculator("Lay's Classic", 1, "oz (28g/about 15 pieces)", 160, 10, 1.5, 0)
         
     #gen_Fruits    
@@ -75,14 +42,59 @@ def enter():
         #gen_Vegetables    
         elif foodinput == "broccoli":
             Calculator("broccoli", 1, "NLEA serving", 50, 0.5, 0.1, 0)
+
+        #Nature Nate's   
+        elif foodinput == "nature nates honey" or foodinput == "03877883044":
+            Calculator("HHOONNEEYY", 1, "tbsp", 60, 0, 0, 0)
                     
         else:
             print "Can't recognize."
         
-        else:
-            print "Empty. Please enter a food."
+    else:
+        print "Empty. Please enter a food."
+def enter():
+    global foodinput
+    foodinput = raw_input("What is the food? ")
+    foodinput = foodinput.lower()
+    search()
+def scanner():
+    # create a Processor
+    proc = zbar.Processor()
+    
+    # configure the Processor
+    proc.parse_config('enable')
+    
+    # initialize the Processor
+    device = '/dev/video1'
+    if len(argv) > 1:
+        device = argv[1]
+    proc.init(device)
+    
+    # setup a callback
+    def my_handler(proc, image, closure):
+        # extract results
+        for symbol in image.symbols:
+            # do something useful with results
+            print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+            if symbol.data == "381371151035":
+                foodinput = "aveeno"
+                
+    proc.set_data_handler(my_handler)
+    
+    # enable the preview window
+    proc.visible = True
+    
+    # initiate scanning
+    proc.active = True
+
+    try:
+        # keep scanning until user provides key/mouse input
+        proc.user_wait()
+    except zbar.WindowClosed, e:
+        if len(foodinput) != 0:
+            search()
 while 1:
-    thingInput = raw_input("Enter S for scanning, enter I for input, and enter Q to quit.")
+    thingInput = raw_input("Enter S for scanning, enter I for input, and enter Q to quit. ")
     thingInput = thingInput.lower()
     if thingInput == 's':
         scanner()
@@ -92,4 +104,3 @@ while 1:
         exit()
     else:
         print "Enter a valid letter"
-    
